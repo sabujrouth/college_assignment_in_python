8a6d6d1fcd7259f2ee46458f5864cf90397e2323
Sabuj <sabujrouth2000@gmail.com>
2023-01-18 19:51:59 +0530

    intersection between two lists

---
+++ 8_intersectionbetweentwolists.py
@@ -0,0 +1,9 @@
+# sample lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+
+# use list comprehension
+intersection = [x for x in list1 if x in list2]
+
+# print intersection
+print(intersection)
